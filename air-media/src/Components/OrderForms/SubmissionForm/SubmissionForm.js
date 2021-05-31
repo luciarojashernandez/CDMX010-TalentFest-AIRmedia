0deadd0e2392aced43719775e8479ca3c0d750a0
Yessica <alvaradoyessica03@gmail.com>
2021-05-31 10:09:48 -0500

    Added forms

---
+++ air-media/src/Components/OrderForms/SubmissionForm/SubmissionForm.js
@@ -0,0 +1,41 @@
+//formulario de envio
+import React, { Fragment } from "react";
+// import FormNav from "../../FormNav/FormNav";
+import {
+    FormDiv,
+    FormName,
+    FormAdress,
+    FormDate,
+    SelectDate,
+    FormPay,
+    FormRadio,
+    FormInput,
+    FormPhone,
+    FormComments
+  } from "../../../Pages/Form/FormElements";
+
+const SubmissionForm = () => {
+  return (
+    <Fragment>
+      {/* <FormNav /> */}
+      <FormDiv>
+        <FormName type="text" name="name" placeholder="Nombre" />
+        <FormAdress type="text" name="name" placeholder="Domicilio" />
+        <FormDate> Día y hora para recibir </FormDate>
+        <SelectDate type="datetime-local" />
+        <FormPay> Forma de pago </FormPay>
+        <FormRadio>
+          <FormInput type="radio" value="Efectivo" />
+          <label for="huey">Efectivo</label>
+          <FormInput type="radio" value="PagoLinea" />
+          <label for="huey">Pago en línea</label>
+        </FormRadio>
+        <FormPhone type="text" name="phone" placeholder="Teléfono" />
+        <FormComments type="text" name="commments" placeholder="Comentarios" />
+      </FormDiv>
+      <button>Confirmar pedido</button>
+    </Fragment>
+  );
+};
+
+export default SubmissionForm;
--- air-media/src/Pages/Form/Form.js
+++ air-media/src/Pages/Form/Form.js
@@ -1,44 +1,27 @@
 import React, { Fragment } from "react";
 import FormNav from "../../Components/FormNav/FormNav";
+import {Link} from 'react-router-dom'
 import {
   FormBtns,
   FormRequestBtn,
   FormPickUpBtn,
-  FormDiv,
-  FormName,
-  FormAdress,
-  FormDate,
-  SelectDate,
-  FormPay,
-  FormRadio,
-  FormInput,
-  FormPhone,
-  FormComments
 } from "./FormElements";
+import SubmissionForm from '../../Components/OrderForms/SubmissionForm/SubmissionForm'
+import FormToCollect from '../../Components/OrderForms/FormToCollect/FormToCollect'
 
 function Form() {
+
+  const condition = true;
+
   return (
     <Fragment>
       <FormNav />
       <FormBtns>
-        <FormRequestBtn>Solicitar envio</FormRequestBtn>
-        <FormPickUpBtn>Recoger pedido</FormPickUpBtn>
+        {condition ?<FormRequestBtn>Solicitar envio</FormRequestBtn>
+        :<FormPickUpBtn>Recoger pedido</FormPickUpBtn>}
       </FormBtns>
-      <FormDiv>
-        <FormName type="text" name="name" placeholder="Nombre" />
-        <FormAdress type="text" name="name" placeholder="Dirección" />
-        <FormDate> Día y hora para recibir </FormDate>
-        <SelectDate type="datetime-local" />
-        <FormPay> Forma de pago </FormPay>
-		<FormRadio>
-			<FormInput type="radio" value='Efectivo'/>
-			<label for="huey">Efectivo</label>
-			<FormInput type="radio" value='pagoLinea'/>
-			<label for="huey">Pago en línea</label>
-		</FormRadio>
-		<FormPhone type="text" name="phone" placeholder="Teléfono"/>
-		<FormComments type="text" name="commments" placeholder="comentarios"/>
-	  </FormDiv>
+      {/* <SubmissionForm /> */}
+      {/* <FormToCollect /> */}
     </Fragment>
   );
 }
