0147c736b1045b2ca0da797830da0db0cdfacf4c
LU <rojaslucia.h@gmail.com>
2021-06-01 16:06:00 -0500

    added forms state, date library, error in the counter function in cart

--- air-media/src/Components/OrderForms/DeliveryForm/DeliveryForm.js
+++ air-media/src/Components/OrderForms/DeliveryForm/DeliveryForm.js
@@ -1,7 +1,8 @@
 //formulario de envio
-import React, { Fragment } from "react";
-
-// import FormNav from "../../FormNav/FormNav";
+import React, { Fragment, useState } from "react";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import TimePicker from 'react-time-picker';
 import {
   FormDiv,
   FormName,
@@ -14,9 +15,74 @@ import {
   FormPhone,
   FormComments,
   FormBtnDelivery,
+  FormSubtotal,
+  FormEnv,
 } from "../../../Pages/Form/FormElements";
 
 const DeliveryForm = () => {
+//DATE & HOUR FUNCTION
+  const [value, onChange] = useState('10:00');
+  const [selectedDate, setSelectedDate] = useState(null);  
+
+//FORM STATE
+   //Se crea un estado vacío para ir llenando
+   const [dataClient, setDataClient] = useState({
+    name: "",
+    adress: "",
+    date: "",
+    hour: "",
+    pagoEfectivo: "",
+    pagoLinea: "",
+    phone: 0,
+    commments: "",
+    subtotal: 0,
+    envio: 30,
+    total: 0,
+  });
+
+  //Se obtienen la relación de datos desde el evento (onChange)
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setDataClient({
+      ...dataClient,
+      [e.target.name]: e.target.value,
+      [e.target.adress]: e.target.value,
+      [e.target.date]: e.target.value,
+      [e.target.hour]: e.target.value,
+      [e.target.pagoEfectivo]: e.target.value,
+      [e.target.pagoLinea]: e.target.value,
+      [e.target.phone]: e.target.value,
+      [e.target.commments]: e.target.value,
+      [e.target.subtotal]: e.target.value,
+      [e.target.envio]: e.target.value,
+      [e.target.total]: e.target.value,
+    });
+  };
+  const sendData = (e) => {
+    //evitar el envío automático
+    e.preventDefault();
+    console.log(dataClient.name +
+        "_" +
+        dataClient.adress +
+        "_" +
+        dataClient.date +
+        "_" +
+        dataClient.pagoEfectivo +
+        "_" +
+        dataClient.pagoLinea +
+        "_" +
+        dataClient.phone +
+        "_" +
+        dataClient.commments +
+        "_" +
+        dataClient.subtotal +
+        "_" +
+        dataClient.total
+    );
+  };
+
+
+  //WA FUNCTION
   function Whatsapp() {
     let restaurantNumber = 525531345330;
 
@@ -41,22 +107,47 @@ const DeliveryForm = () => {
 
   return (
     <Fragment>
+      <form onSubmit={sendData}>
       <FormDiv>
-        <FormName type="text" name="name" placeholder="Nombre" />
-        <FormAdress type="text" name="name" placeholder="Domicilio" />
-        <FormDate> Día y hora para recibir </FormDate>
-        <SelectDate type="datetime-local" />
+        <FormName type="text" name="name" placeholder="Nombre" onChange={handleInputChange}/>
+        <FormAdress type="text" name="adress" placeholder="Domicilio" onChange={handleInputChange}/>
+        {/* <FormDate> Día y hora para recibir </FormDate>
+        <SelectDate type="datetime-local" /> */}
+        <DatePicker
+         name="date"
+         selected={selectedDate}
+         onChange={date => setSelectedDate(date)}
+         placeholderText='dd/mm/aaaa'
+         minDate={new Date()}
+         filterDate={date => date.getDay() !== 6 && date.getDay() !== 0}
+         isClearable
+         dateFormat="dd/MM/yyyy"     
+         />
+         <TimePicker
+        name="hour"
+         onChange={onChange}
+         value={value}
+         minutePlaceholder="h:mm"
+        />
         <FormPay> Forma de pago </FormPay>
         <FormRadio>
-          <FormInput type="radio" value="Efectivo" />
+          <FormInput type="radio" value="Efectivo" name="pagoEfectivo" onChange={handleInputChange} />
           <label for="huey">Efectivo</label>
-          <FormInput type="radio" value="PagoLinea" />
+          <FormInput type="radio" value="PagoLinea" name="pagoLinea" onChange={handleInputChange} />
           <label for="huey">Pago en línea</label>
         </FormRadio>
-        <FormPhone type="text" name="phone" placeholder="Teléfono" />
-        <FormComments type="text" name="commments" placeholder="Comentarios" />
+        <FormPhone type="text" name="phone" placeholder="Teléfono" onChange={handleInputChange} />
+        <FormComments type="text" name="commments" placeholder="Comentarios" onChange={handleInputChange}/>
+        <FormSubtotal type="number" name="subtotal">
+            Subtotal ${0}MXN
+          </FormSubtotal>
+          <FormEnv type="number" name="envio">
+            Costo de envío ${30}MXN
+          </FormEnv>
+          {/* <h2 type="number" name="total">Total: ${0}</h2> */}
       </FormDiv>
-      <FormBtnDelivery>Confirmar pedido</FormBtnDelivery>
+      <FormBtnDelivery type="submit" >Confirmar pedido</FormBtnDelivery>
+      </form>
     </Fragment>
   );
 };
--- air-media/src/Components/OrderForms/PickUpForm/PickUpForm.js
+++ air-media/src/Components/OrderForms/PickUpForm/PickUpForm.js
@@ -1,5 +1,5 @@
 //formulario para recoger
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 // import FormNav from "../../FormNav/FormNav";
 import {
   FormDiv,
@@ -12,24 +12,84 @@ import {
   FormSubtotal,
   FormEnv,
 } from "../../../Pages/Form/FormElements";
+
 const PickUpForm = () => {
+  //FORM STATE
+  //Se crea un estado vacío para ir llenando
+  const [dataClient, setDataClient] = useState({
+    name: "",
+    adress: "",
+    date: "",
+    pagoEfectivo: "",
+    pagoLinea: "",
+    phone: 0,
+    commments: "",
+    subtotal: 0,
+    envio:0,
+    total: 0,
+  });
+
+  //Se obtienen la relación de datos desde el evento (onChange)
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setDataClient({
+      ...dataClient,
+      [e.target.name]: e.target.value,
+      [e.target.adress]: e.target.value,
+      [e.target.date]: e.target.value,
+      [e.target.pagoEfectivo]: e.target.value,
+      [e.target.pagoLinea]: e.target.value,
+      [e.target.phone]: e.target.value,
+      [e.target.commments]: e.target.value,
+      [e.target.subtotal]: e.target.value,
+      [e.target.envio]: e.target.value,
+      [e.target.total]: e.target.value,
+    });
+  };
+
+  const sendData = (e) => {
+    //evitar el envío automático
+    e.preventDefault();
+    console.log(
+      dataClient.name +
+        "_" +
+        dataClient.adress +
+        "_" +
+        dataClient.date +
+        "_" +
+        dataClient.pagoEfectivo +
+        "_" +
+        dataClient.pagoLinea +
+        "_" +
+        dataClient.phone +
+        "_" +
+        dataClient.commments +
+        "_" +
+        dataClient.subtotal +
+        "_" +
+        dataClient.total
+    );
+  };
+
   return (
     <Fragment>
-      {/* <FormNav /> */}
-      <FormDiv>
-        <FormName type="text" name="name" placeholder="Nombre" />
-        <FormDatePick> Día y hora para recibir </FormDatePick>
-        <SelectDatePick type="datetime-local" />
-        <FormPhonePick type="text" name="phone" placeholder="Teléfono" />
-        <FormCommentsPick
-          type="text"
-          name="commments"
-          placeholder="Comentarios"
-        />
-        <FormSubtotal>Subtotal $200MXN</FormSubtotal>
-        <FormEnv>Costo de envio $0MXN</FormEnv>
-      </FormDiv>
-      <FormBtnPick>Confirmar pedido</FormBtnPick>
+      <form onSubmit={sendData}>
+        <FormDiv>
+          <FormName type="text" name="name" placeholder="Nombre" onChange={handleInputChange} />
+          <FormDatePick> Día y hora para recibir </FormDatePick>
+          <SelectDatePick type="datetime-local" name="date" onChange={handleInputChange} />
+          <FormPhonePick type="text" name="phone" placeholder="Teléfono" onChange={handleInputChange}  />
+          <FormCommentsPick
+            type="text"
+            name="commments"
+            placeholder="Comentarios"
+          />
+          <FormSubtotal type="number" name="subtotal" onChange={handleInputChange}>Subtotal ${0}MXN</FormSubtotal>
+          <FormEnv type="number" name="subtotal" onChange={handleInputChange}>Costo de envío ${0}MXN</FormEnv>
+        </FormDiv>
+        {/* <h2 type="number" name="total">Total: ${0}</h2> */}
+        <FormBtnPick type="submit" >Confirmar pedido</FormBtnPick>
+      </form>
     </Fragment>
   );
 };
